d8b9e45752540ab1e514486a89e92b8ea5d23dea
Mark Veltzer <mark.veltzer@gmail.com>
2024-12-19 15:56:10 +0200

    no commit message given

--- exercises/00_install_local/install_spark.bash
+++ exercises/00_install_local/install_spark.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-version="3.5.2"
+version="3.5.3"
 toplevel="spark-${version}-bin-hadoop3"
 if [ ! -d "${HOME}/install" ]
 then
@@ -15,7 +15,8 @@ then
 	rm -rf "${HOME}/install/${toplevel}"
 fi
 
-url="https://dlcdn.apache.org/spark/spark-${version}/spark-${version}-bin-hadoop3.tgz"
+# url="https://dlcdn.apache.org/spark/spark-${version}/spark-${version}-bin-hadoop3.tgz"
+url="https://www.apache.org/dyn/closer.lua/spark/spark-${version}/spark-${version}-bin-hadoop3.tgz"
 # echo "url is [${url}]..."
 curl --location --silent "${url}" | tar xz -C "${HOME}/install"
 ln -s "${HOME}/install/${toplevel}" "${HOME}/install/spark"
