28684f0ed9e740ee08943929de7866ef38a5b2a4
Mark Veltzer <mark.veltzer@gmail.com>
2024-12-19 16:01:10 +0200

    no commit message given

--- exercises/00_install_local/install_spark.bash
+++ exercises/00_install_local/install_spark.bash
@@ -15,8 +15,7 @@ then
 	rm -rf "${HOME}/install/${toplevel}"
 fi
 
-# url="https://dlcdn.apache.org/spark/spark-${version}/spark-${version}-bin-hadoop3.tgz"
-url="https://www.apache.org/dyn/closer.lua/spark/spark-${version}/spark-${version}-bin-hadoop3.tgz"
+url="https://dlcdn.apache.org/spark/spark-${version}/spark-${version}-bin-hadoop3.tgz"
 # echo "url is [${url}]..."
 curl --location --silent "${url}" | tar xz -C "${HOME}/install"
 ln -s "${HOME}/install/${toplevel}" "${HOME}/install/spark"
