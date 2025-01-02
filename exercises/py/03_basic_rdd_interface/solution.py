8236fb38de3b4c5dacbcfc7b72240072baeb6ea4
Mark Veltzer <mark.veltzer@gmail.com>
2025-01-02 15:14:56 +0200

    no commit message given

--- exercises/py/03_basic_rdd_interface/solution.py
+++ exercises/py/03_basic_rdd_interface/solution.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
 
+import sys
 from pyspark import SparkContext, SparkConf
 
 # Initialize Spark
 conf = SparkConf().setAppName("Simple RDD Example").setMaster("local[*]")
 sc = SparkContext(conf=conf)
+# sc.setLogLevel("ERROR")
 
 # Create RDD from a list
-numbers = [1, 2, 3, 4, 5]
+# numbers = [1, 2, 3, 4, 5]
+
+# this is called a python "generator"
+def create_some_numbers():
+    import time
+    for x in range(10000000000):
+        print("im going to sleep...")
+        # time.sleep(5)
+        print(f"giving {x} to pyspark...")
+        yield x
+
+# numbers = [1, 2, 3, 4, 5]
+numbers = range(0, 1000000)
 numbersRDD = sc.parallelize(numbers)
 
 # Let's see what's in it
@@ -15,18 +29,30 @@ print("Elements in RDD:")
 for num in numbersRDD.collect():
     print(num)
 
+# sc.stop()
+# sys.exit(1)
+
 # Create RDD from a list of tuples
 pairs = [("a", 1), ("b", 2), ("c", 3)]
 pairsRDD = sc.parallelize(pairs)
+print(dir(sc))
 
-print("\nPairs in RDD:")
-for pair in pairsRDD.collect():
-    print(f"Key: {pair[0]}, Value: {pair[1]}")
+print("Pairs in RDD:")
+for key, value in pairsRDD.collect():
+    print(f"Key: {key}, Value: {value}")
+
+def do_something_with_value(x):
+    return x+1
+
+def square(x):
+    return x*x
 
 # Simple transformations
-squared = numbersRDD.map(lambda x: x * x)
-print("\nSquared numbers:")
-print(squared.collect())
+# squared = numbersRDD.map(lambda x: x * x)
+squared = numbersRDD.map(square)
+# squared = numbersRDD.map(do_something_with_value)
+print("Squared numbers:")
+print(sum(squared.collect()))
 
 # Don't forget to stop the SparkContext when done
 sc.stop()
---
+++ exercises/py/06_process_a_simple_file/solution.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from pyspark import SparkContext, SparkConf
+
+# Initialize Spark
+conf = SparkConf().setAppName("Sum Odd Numbers").setMaster("spark://localhost:7077")
+sc = SparkContext(conf=conf)
+
+# Set log level to reduce noise
+sc.setLogLevel("ERROR")
+
+# Read the file
+# Note: textFile is the correct method (not sc.read as mentioned in exercise)
+numbers_rdd = sc.textFile("numbers.txt")
+
+# Convert strings to integers and filter odd numbers
+odd_numbers = numbers_rdd \
+    .map(lambda x: int(x.strip())) \
+    .filter(lambda x: x % 2 != 0)
+
+# Calculate sum
+sum_odd = odd_numbers.sum()
+
+print(f"Sum of odd numbers: {sum_odd}")
+
+# Clean up
+sc.stop()
