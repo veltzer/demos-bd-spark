23874ed5b0d1b17d65b31ea2851b74845c4b5a56
Mark Veltzer <mark.veltzer@gmail.com>
2025-01-03 01:04:39 +0200

    no commit message given

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
+
+source scripts/share.sh
+
 export SPARK_MASTER_HOST=localhost
 export SPARK_WORKER_HOST=localhost
 export SPARK_MASTER_WEBUI_HOST=localhost
@@ -6,16 +9,6 @@ export SPARK_WORKER_WEBUI_HOST=localhost
 export SPARK_WORKER_UI_ADDRESS=localhost
 export SPARK_LOCAL_IP=localhost
 export SPARK_PUBLIC_DNS=localhost
-
-function check_spark_master() {
-    if pgrep -f "org.apache.spark.deploy.master.Master" > /dev/null; then
-        echo "Spark master is running"
-        return 0
-    else
-        echo "Spark master is not running"
-        return 1
-    fi
-}
 
 if ! check_spark_master
 then
--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,14 +1,6 @@
 #!/bin/bash -e
 
-function check_spark_master() {
-    if pgrep -f "org.apache.spark.deploy.master.Master" > /dev/null; then
-        echo "Spark master is running"
-        return 0
-    else
-        echo "Spark master is not running"
-        return 1
-    fi
-}
+source scripts/share.sh
 
 if check_spark_master
 then
