19f00c77297ed3bc5c8cede07adab7b48df5db94
Mark Veltzer <mark.veltzer@gmail.com>
2025-01-22 13:55:05 +0200

    no commit message given

---
+++ exercises/pyspark/18_udf/solution.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+# First, let's create some sample data
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
+from pyspark.sql.types import IntegerType
+
+# Initialize Spark session
+spark = SparkSession.builder \
+    .appName("IP Address Analysis") \
+    .getOrCreate()
+
+# Create sample data
+sample_data = [
+    ("server1", "192.168.80.1", "2024-01-22"),
+    ("server2", "10.0.81.34", "2024-01-22"),
+    ("server3", "172.16.82.100", "2024-01-22"),
+    ("server4", "192.168.83.55", "2024-01-22"),
+    ("server5", "10.0.84.89", "2024-01-22")
+]
+
+# Create DataFrame
+df = spark.createDataFrame(sample_data, ["server_name", "ip_address", "date"])
+
+# Method 1: Using UDF in DataFrame API
+def extract_third_octet(ip):
+    """Extract the third number from an IP address."""
+    if ip is None:
+        return None
+    try:
+        return int(ip.split('.')[2])
+    except (IndexError, ValueError):
+        return None
+
+# Register UDF
+get_third_octet = udf(extract_third_octet, IntegerType())
+
+# Use UDF in DataFrame
+print("Method 1: Using DataFrame API")
+df.select("server_name", "ip_address", 
+          get_third_octet("ip_address").alias("third_octet")) \
+  .show()
+
+# Method 2: Using UDF in Spark SQL
+# Register the DataFrame as a temporary view
+df.createOrReplaceTempView("server_ips")
+
+# Register the UDF for SQL
+spark.udf.register("get_third_octet", extract_third_octet, IntegerType())
+
+# Use UDF in SQL query
+print("\nMethod 2: Using Spark SQL")
+spark.sql("""
+    SELECT 
+        server_name,
+        ip_address,
+        get_third_octet(ip_address) as third_octet
+    FROM server_ips
+""").show()
+
+# Sample output will look like:
+# +-----------+---------------+-----------+
+# |server_name|     ip_address|third_octet|
+# +-----------+---------------+-----------+
+# |    server1|   192.168.80.1|        80|
+# |    server2|    10.0.81.34|        81|
+# |    server3|  172.16.82.100|        82|
+# |    server4|  192.168.83.55|        83|
+# |    server5|    10.0.84.89|        84|
+# +-----------+---------------+-----------+
