0b08c05fdc53995c2d137877419ce308f7b71b27
Mark Veltzer <mark.veltzer@gmail.com>
2025-01-29 15:12:38 +0200

    no commit message given

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -2,13 +2,13 @@
 
 source scripts/share.sh
 
-export SPARK_MASTER_HOST=localhost
-export SPARK_WORKER_HOST=localhost
-export SPARK_MASTER_WEBUI_HOST=localhost
-export SPARK_WORKER_WEBUI_HOST=localhost
-export SPARK_WORKER_UI_ADDRESS=localhost
+export SPARK_MASTER_HOST="localhost"
+export SPARK_WORKER_HOST="localhost"
+export SPARK_MASTER_WEBUI_HOST="localhost"
+export SPARK_WORKER_WEBUI_HOST="localhost"
+export SPARK_WORKER_UI_ADDRESS="localhost"
 export SPARK_LOCAL_IP="0.0.0.0"
-export SPARK_PUBLIC_DNS=localhost
+export SPARK_PUBLIC_DNS="localhost"
 
 if ! check_spark_master
 then
