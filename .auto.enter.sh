dec64edcf8b0a35a577a9bdbbb2fb279b65f83d0
Mark Veltzer <mark.veltzer@gmail.com>
2025-02-01 19:17:39 +0200

    no commit message given

--- .auto.enter.sh
+++ .auto.enter.sh
@@ -1,1 +1,5 @@
 export SPARK_MASTER_HOST="spark://localhost:7077"
+# this is local with all cores
+export SPARK_MASTER_HOST="local[*]"
+# this is local with 1 core
+export SPARK_MASTER_HOST="local[1]"
