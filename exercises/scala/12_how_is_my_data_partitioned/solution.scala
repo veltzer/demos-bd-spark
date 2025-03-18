029b16d2facbc6301667a958bc271f831789ca25
Mark Veltzer <mark.veltzer@gmail.com>
2025-03-18 16:22:04 +0200

    no commit message given

---
+++ exercises/scala/12_how_is_my_data_partitioned/solution.scala
@@ -0,0 +1,20 @@
+// Run with: spark-shell -i data-view.scala
+
+val rdd = spark.read.option("header", "true").csv("/tmp/shared/sales_data-csv").rdd
+// val rdd = spark.read.parquet("/tmp/shared/sales_data").rdd
+val numPartitions = rdd.getNumPartitions
+println(s"Number of partitions: $numPartitions")
+val partitionSizes = rdd.glom().map(_.length).collect()
+partitionSizes.zipWithIndex.foreach { case (size, index) =>
+  println(s"Partition $index: $size elements")
+}
+
+val rdd2 = rdd.repartition(numPartitions = 500)
+val numPartitions2 = rdd2.getNumPartitions
+println(s"Number of partitions: $numPartitions2")
+val partitionSizes2 = rdd2.glom().map(_.length).collect()
+partitionSizes2.zipWithIndex.foreach { case (size, index) =>
+  println(s"Partition $index: $size elements")
+}
+
+System.exit(0)
