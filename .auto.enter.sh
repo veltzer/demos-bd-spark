f42eb96de038ab24cf1130d3b91bb223e5b7c463
Mark Veltzer <mark.veltzer@gmail.com>
2025-03-18 14:43:23 +0200

    no commit message given

--- .auto.enter.sh
+++ .auto.enter.sh
@@ -1,5 +1,8 @@
+# the next will connect you to an ALREADY running spark cluster on localhost:7077
 export SPARK_MASTER="spark://localhost:7077"
-# this is local with all cores
+# if you have a remote spark and you know it's IP then you use with
+export SPARK_MASTER="spark://[your ip]:7077"
+# this will launch a new cluster on all cores of the localhost
 export SPARK_MASTER="local[*]"
-# this is local with 1 core
+# same as above but with only 1 core 
 export SPARK_MASTER="local[1]"
